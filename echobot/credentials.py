047b119813a202a8f4d44e067f81098851e56139
proskillowner <116067530+proskillowner@users.noreply.github.com>
2022-10-31 03:19:05 +0900

    Update credentials.py

--- echobot/credentials.py
+++ echobot/credentials.py
@@ -1,3 +1,3 @@
 bot_user_name = "@proskillowner_bot"
 TOKEN = "5520921893:AAG2L-s7XHkPdDXzgh4nHGEt4zfYV9MMMFc"
-URL = "https://proskillowner-echobot2.herokuapp.com/"
+URL = "https://proskillowner-echobot.herokuapp.com/"
